c83b0051626eae6d9bd927876dcb8d51171577cd
ziru0 <ruizharoldandrie@gmail.com>
2024-06-04 14:16:40 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const session = require('express-session');
+const bodyparser = require('body-parser');
+var path = require('path');
+
+const app = express();
+
+
+app.use(session({secret:'sessionsecret777'}));
+app.use(bodyparser.urlencoded({extended:true}));
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req,res) => {
+    res.send('douglas ♥ cendy')
+})
+
+app.listen(5500,()=>{
+    console.log("Server listening at port 8080");
+})
